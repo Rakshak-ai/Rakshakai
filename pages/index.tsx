370a8b17eca6e45d5206abd0d493b94971ce163f
Rakshak-ai <rakshakaisos@gmail.com>
2025-07-26 07:58:16 +0530

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Donate from "@/components/Donate";
+
+export default function Home() {
+  return (
+    <>
+      {/* other sections */}
+      <Donate />
+      {/* more sections */}
+    </>
+  );
+}
